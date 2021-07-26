19d680dec0fdd5f5a612c9eee1880c772ea8bbf0
pradosh2008 <talk2cetian@gmail.com>
2021-07-26 11:07:19 +0530

    Added a new file hello_in_japanese.py Changed an existing file hello_in_english.py

--- hello_in_english.py
+++ hello_in_english.py
@@ -1,2 +1,4 @@
 input("what's the language you want to print hello?\n")
 print("Hello Everyone")
+
+print("In the morning , we say good morning")
---
+++ hello_in_japanese.py
@@ -0,0 +1,2 @@
+input("what's the language you want to print hello?\n")
+print("Kon'nichiwa, min'na")
